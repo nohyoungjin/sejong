64f6f289e4fad3becc205a8b98367f1526e3028d
nohyoungjin <45346737+nohyoungjin@users.noreply.github.com>
2020-11-04 17:57:13 +0900

    Update 22

---
+++ inc/menu_01.js
@@ -0,0 +1,9 @@
+str = ''
+str += '<ul class="depth3 gray">'
+str += ''
+str += '	<li class="on w50"><a href="sejong_local.html">세종형 로컬푸드</a></li>'
+str += '	<li class="w50"><a href="sejong_why.html">왜 로컬푸드인가?</a></li>'
+str += ''
+str += '</ul>'
+
+document.write(str);
